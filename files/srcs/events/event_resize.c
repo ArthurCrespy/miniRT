d985985cdb88aa47dc7d28d88c0e4cbcf501394d
CRESPY Arthur <arthur@crespy.eu>
2023-11-19 22:47:31 +0100

    Update for handling resizing

--- files/srcs/events/event_resize.c
+++ files/srcs/events/event_resize.c
@@ -1,22 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mlx_loop.c                                         :+:      :+:    :+:   */
+/*   event_resize.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/19 15:02:08 by acrespy           #+#    #+#             */
-/*   Updated: 2023/11/19 15:02:11 by acrespy          ###   ########.fr       */
+/*   Created: 2023/11/19 17:53:32 by acrespy           #+#    #+#             */
+/*   Updated: 2023/11/19 17:53:36 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../includes/miniRT.h"
 
-void	mlx_hook_loop(t_data *data)
+int	event_resize(t_data *data)
 {
-	mlx_loop_hook(data->mlx->mlx, (int (*)(void)) mlx_render_frame, data);
-	mlx_hook(data->mlx->mlx_win, 2, 1L << 0, (int (*)(void)) event_key, data);
-	mlx_hook(data->mlx->mlx_win, 25, 1L << 18, (int (*)(void)) event_resize, data);
-	mlx_hook(data->mlx->mlx_win, 17, 1L << 17, (int (*)(void)) mlx_hook_exit, data);
-	mlx_loop(data->mlx->mlx);
+	// ?
+	mlx_render_frame(data);
+	return (0);
 }
