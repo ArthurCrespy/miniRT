1e4fbc2657fbaf0901c93cd7ee4d8e01f2cf0f8c
CRESPY Arthur <arthur@crespy.eu>
2023-11-19 16:38:29 +0100

    Update main.c

--- files/srcs/main.c
+++ files/srcs/main.c
@@ -12,54 +12,23 @@
 
 #include "./../includes/miniRT.h"
 
-int	key_press(int keycode, void *data)
-{
-	(void)data;
-	if (keycode == 65307)
-		exit(0);
-	return (0);
-}
 
-int	mlx_hook_exit(void *data)
-{
-	(void)data;
-	exit(0);
-}
 
-int main(void)
+int	main(int argc, char **argv, char **envp)
 {
-	void		*mlx;
-	void		*mlx_win;
-	int         win_width;
-	int         win_height;
-	void		*img;
+	t_data	data;
 
 	printf("Hello, World!\n");
 
-	mlx = NULL;
-	mlx_win = NULL;
-	img = NULL;
+	args_check(argc, argv, envp);
+	file_save(&data, argv[1]);
 
-	mlx = mlx_init();
-	if (!mlx)
-		return (1);
-	mlx_get_screen_size(mlx, &win_width, &win_height);
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello, World!");
-	if (!mlx_win)
-		return (1);
+	for (int i = 0; data.file->content[i]; i++)
+		printf("%s\n", data.file->content[i]);
 
-	img = mlx_new_image(mlx, 1920, 1080);
-	if (!img)
-		return (1);
-
-	mlx_put_image_to_window(mlx, mlx_win, img, 0, 0);
-
-	mlx_hook(mlx_win, 2, 1L << 0, (int (*)(void)) key_press, NULL);
-	mlx_hook(mlx_win, 17, 1L << 17, (int (*)(void)) mlx_hook_exit, NULL);
-	mlx_loop(mlx);
-
-	mlx_destroy_display(mlx);
-	free(mlx);
+	mlx_initialize_win(&data);
+	mlx_hook_loop(&data);
+	mlx_free(&data);
 
 	return (0);
 }
