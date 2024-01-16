d2568d05efc426a62e4fb3dd3da9b7bc10db70be
Damien KERAUDREN <dkeraudr@bess-f4r2s5.clusters.42paris.fr>
2024-01-16 19:47:05 +0100

    Added common structs

--- includes/miniRT.h
+++ includes/miniRT.h
@@ -6,7 +6,7 @@
 /*   By: dkeraudr <dkeraudr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 15:14:02 by dkeraudr          #+#    #+#             */
-/*   Updated: 2024/01/11 19:00:48 by dkeraudr         ###   ########.fr       */
+/*   Updated: 2024/01/16 19:42:16 by dkeraudr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,41 +15,14 @@
 
 # include <../minilibx-linux/mlx.h>
 # include <../libft/includes/libft.h>
+# include "structs.h"
 # include <stdio.h>
 # include <stdlib.h>
 
 # define WIDTH 512
 # define HEIGHT 512
 
-typedef struct s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
 
-typedef struct s_vars {
-	void	*mlx;
-	void	*win;
-}				t_vars;
-
-typedef struct s_minirt {
-	t_data		*img_data;
-	t_vars		*vars;
-}				t_minirt;
-
-typedef struct s_color {
-	double	chan_1;
-	double	chan_2;
-	double	chan_3;
-}				t_color;
-
-typedef struct s_vector {
-	double	x;
-	double	y;
-	double	z;
-}				t_complex;
 
 int	exit_program(t_minirt *mini_struct);
 int	handle_keys(int keycode, t_minirt	*mini_struct);
