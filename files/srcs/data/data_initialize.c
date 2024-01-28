11b2c72d5e11f89d29f04e6f06986fcceebf1b81
dkeraudr <dkeraudr@student.42.fr>
2024-01-28 16:26:07 +0100

    Fixed data initialization

--- files/srcs/data/data_initialize.c
+++ files/srcs/data/data_initialize.c
@@ -6,15 +6,27 @@
 /*   By: dkeraudr <dkeraudr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 07:46:43 by acrespy           #+#    #+#             */
-/*   Updated: 2024/01/16 21:02:31 by dkeraudr         ###   ########.fr       */
+/*   Updated: 2024/01/28 16:25:26 by dkeraudr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../../includes/miniRT.h"
+
+void	init_scene(t_scene *scene)
+{
+	scene->camera = NULL;
+	scene->ambient = NULL;
+	scene->lights = NULL;
+	scene->objects = NULL;
+}
 
 void	data_initialize(t_minirt *data)
 {
 	data->mlx = malloc(sizeof(t_mlx));
 	if (!data->mlx)
 		ft_exit("Error: malloc failed\n", 1);
+	data->scene = malloc(sizeof(t_scene));
+	if (!data->scene)
+		ft_exit("Error: malloc failed\n", 1);
+	init_scene(data->scene);
 }
