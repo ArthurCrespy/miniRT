6d846cedd3970a64f8adc01b4a349bf1de6ba221
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-01-29 15:28:17 +0100

    Ray operations

--- files/srcs/ray/ray_transform.c
+++ files/srcs/ray/ray_transform.c
@@ -1,22 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ray_create.c                                       :+:      :+:    :+:   */
+/*   ray_transform.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/20 13:39:12 by acrespy           #+#    #+#             */
-/*   Updated: 2024/01/20 13:39:12 by acrespy          ###   ########.fr       */
+/*   Created: 2024/01/29 15:16:09 by acrespy           #+#    #+#             */
+/*   Updated: 2024/01/29 15:16:09 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../../includes/miniRT.h"
 
-t_ray	ray_new(t_point origin, t_vector direction)
+t_ray	ray_transform(t_ray r, t_matrix m)
 {
-    t_ray	ray;
-
-    ray.origin = origin;
-    ray.direction = direction;
-    return (ray);
+    return (ray_new(matrix_tuple_mult(m, r.origin),matrix_tuple_mult(m, r.direction)));
 }
