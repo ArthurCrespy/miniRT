7ad6360428fa90426dc2f39b92b1ddf4a9cb2169
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-03-08 02:25:56 +0100

    Minor change in if tree of intersect_with

--- files/srcs/intersections/intersect.c
+++ files/srcs/intersections/intersect.c
@@ -37,10 +37,10 @@ double	*intersect_with(t_hittable *obj, t_ray ray)
 	// ft_print_matrix(*invert_transform);
 	if (obj->type == SPHERE)
 		return (intersect_with_sphere(ray_transformed));
-	else if (obj->type == CYLINDER)
-		return (intersect_with_cylinder(obj, ray_transformed));
-	else if (obj->type == PLANE)
+	if (obj->type == PLANE)
 		return (intersect_with_plane(ray_transformed));
+	if (obj->type == CYLINDER)
+		return (intersect_with_cylinder(obj, ray_transformed));
 	return (NULL);
 }
 
