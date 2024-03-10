aaf074b6fd4f60bd6152d68937c93008e468a59e
dkeraudr <dkeraudr@student.42.fr>
2024-03-10 18:09:30 +0100

    Modified test_main

--- files/srcs/main_tests.c
+++ files/srcs/main_tests.c
@@ -6,11 +6,11 @@
 /*   By: dkeraudr <dkeraudr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 20:36:43 by dkeraudr          #+#    #+#             */
-/*   Updated: 2024/03/07 20:37:00 by dkeraudr         ###   ########.fr       */
+/*   Updated: 2024/03/09 16:55:38 by dkeraudr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "miniRT.h"
+#include "miniRT_test.h"
 
 void	do_tests(void);
 // void	do_lighting_00(void);
@@ -19,7 +19,8 @@ void	test_precompute_intersections(void);
 
 int	main(void)
 {
-	do_tests();
+	// do_tests();
+	test_shadows_02();
 	// test_world_intersections();
 	// test_precompute_intersections();
 	return (1);
