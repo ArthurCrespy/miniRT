8040e3e607b7db1dc456665b27ad46f4ff606aa7
CRESPY Arthur <arthur@crespy.eu>
2024-03-22 14:40:01 +0100

    fixup! Fix norm issues

--- srcs/intersections/cylinder_intersection.c
+++ srcs/intersections/cylinder_intersection.c
@@ -21,19 +21,19 @@ double	*intersect_with_cylinder(t_hittable *cy, t_ray ray, double c, double d)
 	a = pow(ray.direction.x, 2) + pow(ray.direction.z, 2);
 	b = 2 * (ray.origin.x * ray.direction.x + ray.origin.z * ray.direction.z);
 	c = pow(ray.origin.x, 2) + pow(ray.origin.z, 2) - 1;
-	discriminant = pow(b, 2) - 4 * a * c;
-	if (discriminant < 0)
+	d = pow(b, 2) - 4 * a * c;
+	if (d < 0)
 		return (NULL);
 	intersections = malloc(sizeof(double) * 2);
 	if (!intersections)
 		return (NULL);
-	intersections[0] = (-b - sqrt(discriminant)) / (2 * a);
-	intersections[1] = (-b + sqrt(discriminant)) / (2 * a);
+	intersections[0] = (-b - sqrt(d)) / (2 * a);
+	intersections[1] = (-b + sqrt(d)) / (2 * a);
 	a = ray.origin.y + intersections[0] * ray.direction.y;
 	b = ray.origin.y + intersections[1] * ray.direction.y;
 	if ((a < -cy->height / 2 || a > cy->height / 2)
 		&& (b < -cy->height / 2 || b > cy->height / 2))
-		return (free(intersections));
+		return (free(intersections), NULL);
 	else if (a < -cy->height / 2 || a > cy->height / 2)
 		intersections[0] = intersections[1];
 	else if (b < -cy->height / 2 || b > cy->height / 2)
