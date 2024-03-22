254837585e5f8be32576a7bed6faa79b2e5343a3
Damien KERAUDREN <dkeraudr@bess-f4r4s4.clusters.42paris.fr>
2024-03-22 20:38:03 +0100

    Fixed norm

--- srcs/mlx/mlx_render.c
+++ srcs/mlx/mlx_render.c
@@ -6,7 +6,7 @@
 /*   By: dkeraudr <dkeraudr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 15:00:18 by acrespy           #+#    #+#             */
-/*   Updated: 2024/03/20 21:12:49 by dkeraudr         ###   ########.fr       */
+/*   Updated: 2024/03/22 20:37:39 by dkeraudr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int	mlx_render_frame(t_minirt *data)
 	percent = get_percent((double)data->pxl_rendered * 100
 			/ (data->mlx->win_width * data->mlx->win_height));
 	if ((double)data->pxl_rendered * 100
-			/ (data->mlx->win_width * data->mlx->win_height) != 100)
+		/ (data->mlx->win_width * data->mlx->win_height) != 100)
 		mlx_string_put(data->mlx->mlx, data->mlx->mlx_win,
 			50, 50, 0x00FFFFFF, percent);
 	free(percent);
